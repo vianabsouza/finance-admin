c5c853a87f60ae1112934f7fc1e5c2a82f7cf837
Barbara Viana <vianabsouza@gmail.com>
2024-07-25 17:09:00 -0300

    feat: implement logic to read, write and create accounts

--- src/accounts/accounts.service.ts
+++ src/accounts/accounts.service.ts
@@ -1,4 +1,30 @@
 import { Injectable } from '@nestjs/common';
+import { Account } from './account.model';
+import path from 'path';
+import fs from 'fs';
 
 @Injectable()
-export class AccountsService {}
+export class AccountsService {
+  private readonly filePath = path.resolve('src/accounts/accounts.json');
+
+  private readAccounts(): Account[] {
+    const data = fs.readFileSync(this.filePath, 'utf8');
+    return JSON.parse[data] as Account[]
+  }
+
+  private writeAccounts(accounts: Account[]): void {
+    fs.writeFileSync(this.filePath, JSON.stringify(accounts, null, 2), 'utf8');
+  }
+
+  createAccount(name: string, balance: number): Account {
+    const accounts = this.readAccounts();
+    const newAccount = {
+      id: accounts.length > 0 ? accounts[accounts.length - 1].id  + 1 : 1,
+      name,
+      balance
+    }
+    accounts.push(newAccount);
+    this.writeAccounts(accounts);
+    return newAccount;
+  }
+}
