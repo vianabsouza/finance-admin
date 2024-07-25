1273af35c408f0c2f816c04c6a1515ddc4fecf4c
Barbara Viana <vianabsouza@gmail.com>
2024-07-25 18:41:36 -0300

    feat: implement accounts controller with post method

--- src/accounts/accounts.controller.ts
+++ src/accounts/accounts.controller.ts
@@ -1,4 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { AccountsService } from './accounts.service';
+import { Account } from './account.model';
 
 @Controller('accounts')
-export class AccountsController {}
+export class AccountsController {
+  constructor(private readonly accountsService: AccountsService) {
+  }
+
+  @Post()
+  createAccount(@Body('name') name: string, @Body('balance') balance: number): Account {
+    return this.accountsService.createAccount(name, balance);
+  }
+}
